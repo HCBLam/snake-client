baad83643231ef8211a51e2dd7b1b4e33926a125
Hasan Lam <hcb.lam@gmail.com>
2021-09-22 20:00:43 +0000

    Added client module

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: 50542
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+    // event handler:  what to do when first connected
+    conn.on('connect', () => {
+      console.log('I am now connected!');
+    });
+
+    // event handler:  what to do when receiving data from the server
+    conn.on('data', (data) => {
+      console.log('Snek says:  ', data);
+    });
+
+  return conn;
+};
+
+module.exports = { connect };

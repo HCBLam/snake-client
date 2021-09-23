104ae095288ef14fe936e999442889ce97eb1cd4
Hasan Lam <hcb.lam@gmail.com>
2021-09-23 01:40:36 +0000

    Added keyboard inputs

--- input.js
+++ input.js
@@ -1,15 +1,41 @@
 const net = require("net");
 
 
+// Stores the active TCP connection object
+let connection;
+
+
 // This defines the user input keys and what they do
 const handleUserInput = function (key) {
+  // binds the Ctrl + c key combo to exit the program
   if (key === '\u0003') {
     process.exit();
+  }
+
+  // to log which key was pressed
+  console.log('The key just pressed is:  ', key);
+
+  // binds the w key to move up
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  // binds the a key to move left
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  // binds the s key to move down
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  // binds the d key to move right
+  if (key === 'd') {
+    connection.write('Move: right');
   }
 }
 
 // This sets up stdin and the UI to accept user input
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
